63352c3307d4ee27457af98e17ebd78b50b43d65
KarmaSora <karammtr@gmail.com>
2023-11-15 14:32:27 +0100

    improved the .gitignore

---
+++ IntroModule2/ClassA.cpp
@@ -0,0 +1,12 @@
+#include "ClassA.h"
+
+string ClassA::getName()
+{
+    return name;
+}
+
+void ClassA::setName(string name)
+{
+    this->name = name;
+}
+
---
+++ IntroModule2/ClassA.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+using namespace std;
+class ClassA {
+
+private:
+	string name;
+
+public:
+
+	string getName();
+	void setName(string name);
+
+};
