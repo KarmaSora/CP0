b8ba6f3a1ae762c5d7f950335f592ec091a733eb
KarmaSora <karammtr@gmail.com>
2023-11-16 12:40:22 +0100

    test

---
+++ OOPCPP/OOPCPP.cpp
@@ -0,0 +1,19 @@
+// OOPCPP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+/*
+
+Klass och object
+
+klass,medlemsvariabler och medlemsfunctioner
+inkapsling och information hiding
+konstructur och destructor
+*/
