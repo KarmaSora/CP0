8fc7662b2e50bf459eeae1c3f09b4ab3d80ead20
KarmaSora <karammtr@gmail.com>
2023-11-16 20:10:23 +0100

    work in progress

--- trainingAssignment/ArrayAndPointer.cpp
+++ trainingAssignment/ArrayAndPointer.cpp
@@ -1,4 +1,5 @@
 #include <iostream>;
+#include <string>
 
 using namespace std;
 
@@ -27,13 +28,18 @@ int calcVocalsInStirng(string& text) {
 	return count;
 }
 
-int calcNumUnder500() {
+//int calcNumUnder500() {
+//
+//	cout << "enter the size of wanted list..>" << endl;
+//	int numberOfElements;
+//	cin >> numberOfElements;
+//	cout << numberOfElements << endl;
+//
+//}
 
-	cout << "enter the size of wanted list..>" << endl;
-	int numberOfElements;
 
 
-}
+
 int main() {
 	/*string a = "this text has 4 spaces!";
 	calcSpacesInString(a);*/
@@ -41,7 +47,39 @@ int main() {
 	/*string b = "kkeKe aKe 8 kokakk MMeMMMme";
 	calcVocalsInStirng(b);*/
 
-	calcNumUnder500();
+	srand(time(NULL));
+	string* stringArray = new string[10];
+	stringArray[0] = "Zero";
+	stringArray[1] = "One";
+	stringArray[2] = "Two";
+	stringArray[3] = "Three";
+	stringArray[4] = "Four";
+	stringArray[5] = "Five";
+	stringArray[6] = "Sex";
+	stringArray[7] = "Seven";
+	stringArray[8] = "Eight";
+	stringArray[9] = "Nine";
+
+	
+
+	for (int i = 0; i < 10; ++i) {
+		int index1 = rand() % 10;
+		int index2 = rand() % 10;
+		swap(stringArray[index1], stringArray[index2]);
+	}
+
+	// Print the contents of the array
+	cout << "Shuffled content of the array: ";
+	for (int i = 0; i < 10; ++i) {
+		cout << stringArray[i] << " ";
+	}
+	cout << endl;
+
+	// Deallocate the memory on the heap
+	delete[] stringArray;
+
+
+	//calcNumUnder500();
 
 	return 0;
 }
