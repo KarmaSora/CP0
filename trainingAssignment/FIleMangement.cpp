f09e48253cd53b57fdd63c0c05a5b92031dc6821
KarmaSora <karammtr@gmail.com>
2023-11-20 11:55:19 +0100

    starting with classes, in OOP

--- trainingAssignment/FIleMangement.cpp
+++ trainingAssignment/FIleMangement.cpp
@@ -5,10 +5,11 @@
 void readFile(std::string fileName) {
 	using namespace std;
 
+
+	/* */
+	//works on singel words, Must know line count!
 	ifstream file;
 	file.open(fileName);
-
-
 	if (file.is_open()) {
 
 		string test;
@@ -17,33 +18,50 @@ void readFile(std::string fileName) {
 		file >> lineCount;
 		for (int i = 0; i < lineCount; i++) {
 			file >> test;
-			cout << test;
+			cout << test<<endl;
+		}
+	}
+	file.close();
+	
+
+
+	/*
+//works on entire lines, not knowing line or word count
+	ifstream file;
+	file.open(fileName);
+	if (file.is_open()) {
+
+		string test;
+		while (getline(file, test)) {
+			cout << test << endl;
 		}
 	}
 	file.close();
 
 }
+*/
 
+}
 
+void writeFile(std::string filename, std::string toWrite) {
 
-//	ifstream file;
-//	file.open(fileName);
-//	if (file.is_open()) {
-//
-//		string test;
-//		while (getline(file, test)) {
-//			cout << test << endl;
-//		}
-//	}
-//	file.close();
-//
-//}
+	std::ofstream file;
+	file.open(filename);
+	if (file.is_open()) {
+
+		file<< toWrite;
+
+	file.close();
+	}
+	
+}
+
 
 
 int main() {
 
-	readFile("karma.txt");
-
+	//readFile("karma.txt");
+	writeFile("testing.txt","karma was here");
 }
 
 
